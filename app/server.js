d63fb81871fa715e3e77abd3088538dcb9a68d5d
regonza <gonza181092@gmail.com>
2023-01-27 00:04:23 -0300

    feat: add routing (proto-01)

--- app/server.js
+++ app/server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 
+const userRouters = require('../app/routes/user');
+
 const app = express();
 
 const port = 3001;
 
-app.get('/', (req, res) => {
-    res.send({
-        data: 'Hello world!'
-    });
-});
+app.use(userRouters)
 
 app.listen(port, () => {
     console.log(`🚀 app ready on port: ${port}`);
